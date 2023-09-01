56a013fdd5c87bab948406300055f735d41a6c1a
NEWHELL69 <kushagra0304@gmail.com>
2023-09-01 11:37:47 +0530

    Exercises 4.18.-4.22.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,5 @@
 const blogRouter = require('express').Router();
-const { default: mongoose } = require('mongoose');
 const Blog = require('../models/blog');
-const User = require('../models/user');
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('userId');
@@ -9,17 +7,8 @@ blogRouter.get('/', async (request, response) => {
   response.json(blogs);
 });
 
-// Adding a random user to a blog as per Excersise 4.17
-const assignRandomUser = async (request, response, next) => {
-  const usersCount = await User.countDocuments();
-  const randomIndex = Math.floor(Math.random() * usersCount);
-  const randomUser = await User.findOne().skip(randomIndex);
-  request.randomUser = randomUser;
-  next();
-};
-
-blogRouter.post('/', assignRandomUser, async (request, response) => {
-  const { body, randomUser } = request;
+blogRouter.post('/', async (request, response) => {
+  const { body, user } = request;
 
   if (!body.title || !body.url) {
     response.status(400).json({
@@ -31,27 +20,46 @@ blogRouter.post('/', assignRandomUser, async (request, response) => {
 
   const blogData = {
     ...body,
-    id: (new mongoose.Types.ObjectId()).toString(),
-    userId: randomUser.id, // Assigning user to blog
+    userId: user.id, // Assigning user to blog
   };
 
   const blog = new Blog(blogData);
   await blog.save();
 
   // Update user with the new blog post
-  randomUser.blogs.push(blog.id); // Assigning blog to user
-  await randomUser.save();
+  user.blogs.push(blog.id); // Assigning blog to user
+  await user.save();
 
   response.status(201).json(blog);
 });
 
 blogRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id);
+  const { user } = request;
+  const blogId = request.params.id;
+
+  if (!user.blogs.includes(blogId)) {
+    response.status(401).json({ error: 'Blog does not belong to user' });
+    return;
+  }
+
+  await Blog.findByIdAndDelete(blogId);
+
+  const index = user.blogs.indexOf(blogId);
+  if (index > -1) { // only splice array when item is found
+    user.blogs.splice(index, 1); // 2nd parameter means remove one item only
+  }
+  await user.save();
+
   response.status(204).end();
 });
 
 blogRouter.put('/:id', async (request, response) => {
-  const { body } = request;
+  const { body, user } = request;
+
+  if (!user.blogs.includes(request.params.id)) {
+    response.status(401).json({ error: 'Blog does not belong to user' });
+    return;
+  }
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, body, { new: true });
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,6 @@
-/* eslint-disable consistent-return */
 const logger = require('./logger');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method);
@@ -17,6 +18,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' });
   } if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
+  } if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: error.message });
   }
 
   next(error);
@@ -26,8 +29,32 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '');
+  }
+
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+  if (!decodedToken.id) {
+    response.status(401).json({ error: 'token invalid' });
+    return;
+  }
+
+  request.user = await User.findById(decodedToken.id);
+
+  next();
+};
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
+  userExtractor,
 };
