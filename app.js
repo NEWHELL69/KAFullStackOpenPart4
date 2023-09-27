b5ce00c0d80a3e0e70206b43a8a9a8b68b25a24b
NEWHELL69 <kushagra0304@gmail.com>
2023-09-27 11:11:42 +0530

    changes for part 5

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const cors = require('cors');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
 const middleware = require('./utils/middleware');
-const logger = require('./utils/logger');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const testingRouter = require('./controllers/testing');
 
 const app = express();
 
@@ -15,7 +15,7 @@ mongoose.set('strictQuery', false);
 console.log('connecting to', config.MONGODB_URI);
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB');
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message);
@@ -26,6 +26,9 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter);
+}
 app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
